0983127af0bf75b63e8d7a80f7555ee95e31418a
shivamjaiswal2200@gmail.com <shivamjaiswal2200@gmail.com>
2023-10-15 20:21:02 +0530

    done

---
+++ src/Images.js
@@ -0,0 +1,19 @@
+const images = [{
+	id: 1,
+	src: "https://picsum.photos/200/300",
+	alt: "Image 1"
+},
+{
+	id: 2,
+	src: "https://picsum.photos/200/301",
+	
+	alt: "Image 2 "
+},
+{
+	id: 3,
+	src: "https://picsum.photos/200/302",
+
+	alt: "Image 3"
+}
+];
+export default images;
